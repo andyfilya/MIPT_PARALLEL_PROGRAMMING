54ac614dcc011fdf5e3581a1c5b0072560bd47ac
Andrey Filkin <liamo19@mail.ru>
2024-09-27 16:13:42 +0300

    default multiply matrices

--- main.c
+++ main.c
@@ -52,12 +52,46 @@ void PrintMatrix(Matrix* m) {
   }
 }
 
+
+// MultiplyMatrices - функция умножения двух матриц, которая возвращает
+// как результат новую матрицу, являющуюся перемножением двух других.
+// Данная функция не является оптимальной - здесь все сделано " в лоб".
+Matrix* MultiplyMatrices(Matrix* firstMatrix, Matrix* secondMatrix) {
+  // обработка стандартной ошибки, в случае если количество столбцов
+  // первой матрицы не совпадает с количеством строк во второй
+    if (firstMatrix->cols != secondMatrix->rows) {
+        printf("error: matrix multiplication");
+        return NULL;
+    }
+    // результирующая матрица, которая будем отображать результат
+    // перемножения двух матриц C = AB
+    Matrix* resultMatrix = CreateMatrix(firstMatrix->rows, secondMatrix->cols);
+    // первым делом будем делать "в лоб" - не думая о кеше, и так далее:
+    for (int i = 0; i < firstMatrix->rows; i++) {
+        for (int j = 0; j < secondMatrix->cols; j++) {
+            resultMatrix->data[i][j] = 0; // Инициализируем элемент
+            for (int k = 0; k < firstMatrix->cols; k++) {
+                resultMatrix->data[i][j] += firstMatrix->data[i][k] * secondMatrix->data[k][j];
+            }
+        }
+    }
+    // возвраещем результат C=AB
+    return resultMatrix;
+}
+
 int main(int argc, char **argv) {
+  Matrix* a = CreateMatrix(2, 2);
+  Matrix* b = CreateMatrix(2, 2);
 
-  Matrix* m = CreateMatrix(15, 15);
-  FillMatrix(m);
-  PrintMatrix(m);
-  FreeMatrix(m);
+  FillMatrix(a);
+  FillMatrix(b);
 
+  PrintMatrix(a);
+  printf("\n\n\n\n");
+  PrintMatrix(b);
+  printf("\n\n\n\n");
+  Matrix* c = MultiplyMatrices(a, b);
+
+  PrintMatrix(c);
   return 0;
 }
